ae1e316ec2246f51f8ace5568dc6f093419bbf21
sabriku <sabri@sabrikustur.net>
2026-06-17 06:09:47 +0300

    Bump page navigation cache

--- public/cleanup-fix.js
+++ public/cleanup-fix.js
@@ -9,7 +9,7 @@
   }
 
   function loadNavigationHelpers() {
-    loadScriptOnce('tb-main-radar-tabs-loader-v12', '/radar-main-tabs.js?v=20260524-12');
+    loadScriptOnce('tb-main-radar-tabs-loader-v13', '/radar-main-tabs.js?v=20260524-13');
   }
 
   function cleanupBtn() { return document.getElementById('tb-cleanup'); }
